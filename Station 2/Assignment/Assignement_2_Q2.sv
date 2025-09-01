7a7e6fbbb23eccb1b1df776a7267e69d5d972903
Mahmoud Ismail <mahmoudismailkn@gmail.com>
2025-09-01 04:52:50 +0300

    Assignment 2 Q2

---
+++ Station 2/Assignment/Assignement_2_Q2.sv
@@ -0,0 +1,26 @@
+module Assignment_2_Q2 ();
+
+bit [15:0] arr1 [0:7];
+integer arr2 [8];
+bit  [15:0] dyn_arr [];
+initial begin
+
+    foreach (arr1[i]) begin
+        arr1[i] = i;
+    end
+    $display ("arr1 : %p" , arr1);
+    foreach (arr2[i]) begin
+        arr2[i] = 2*i;
+    end
+    $display ("arr2 : %p" , arr2);
+
+    dyn_arr = new[8] (arr1); //unpacked size will be dynamic.
+    $display ("dyn_arr : %p" , dyn_arr);
+end
+
+
+
+
+
+
+endmodule
